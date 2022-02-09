967b26f4dfacbf6d5862b847c65b399ab9069eb7
doomnet12345 <76430187+doomnet12345@users.noreply.github.com>
2022-02-08 23:38:30 -0500

    Delete scroll.js

--- scroll.js
+++
@@ -1,6 +0,0 @@
-function scrollToLearnMore(){const element=document.getElementById("learnMoreSection");console.log(window)
-element.scrollIntoView({behavior:"smooth"})
-stop()}
-function scrollToPricing(){const element=document.getElementById("pricingSection");console.log(window)
-element.scrollIntoView({behavior:"smooth"})
-stop()}
