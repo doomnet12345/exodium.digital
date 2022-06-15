fde320e84aa4b26d16d7b08828041048c0cefdca
doomnet12345 <76430187+doomnet12345@users.noreply.github.com>
2022-06-15 01:16:34 -0400

    Update rules.php

--- rules.php
+++ rules.php
@@ -1,1 +1,186 @@
+Rules of RevelixRP
+General rules A.
+a1.1 Cheating, Macros, Exploiting or Crashing the server, is not permitted.
+
+a1.2 Random Death Match(RDM) and Random Arrest(RDA) are not allowed.
+
+a1.3 NLR: You must wait at least 2 minutes before returning to the area you died in and you may not interact with the player's RP, E.g. Dying at the bank and then going to the corrupt banker.
+
+a1.4 Discrimination (E.g. Hate Speech or Harassment) is not allowed.
+
+a1.5 Scamming or abusing a glitch/loophole is not tolerated.
+
+a1.6 Trolling will not be accepted.
+
+a1.7 As a player in an admin situation, do not interrupt any other sits or attempt to walk away.
+
+a1.8 Staffs say is final.
+
+a1.9 Chat and Mic spamming is not allowed (including music, excluding DJ job)
+
+a2.0 Prop pushing and prop surfing are not allowed.
+
+a2.1 You must advert when mugging, kidnapping, stealing, warning someone to go away and as a member of pd when telling someone to put their gun away.
+
+a2.2 When warning someone to go away etc or to put their gun away, you must advert the warning 2 times with at least 5 seconds in between each advert.
+
+a2.3 You may only advert warning to kill for valid reasons and not if it was unprovoked, the player also has to be within 1 meter of you (if they keep coming back you can kill them)
+
+a2.4 Your adverts should not contain unnecessary details and should only include one thing E.g: Steal or Mug not both.
+
+a2.5 Your adverts must not contain inappropriate language and must be (for RP situations) yellow.
+
+a2.6 Roleplay in spawn/safe zone is not permitted.(Roleplaying in mining area is allowed) (NOT IN GAME)
+
+a2.7 Do not abuse safe zones to escape or avoid a roleplay situation.
+
+a2.8 If you have a 5-star wanted level, you can be arrested in the spawn area. This includes the safezone.(NOT IN GAME)
+
+a2.9 You may only advert what is needed without any added unnecessary detail, E.g. Warning go away 1/2/3.
+
+a3.0 You may not have multi adverts. E.g: Raid/Mug/Steal/Warning 1/2/3
+
+a3.1 Angle abuse will not be tolerated. (Viewing the other person from an angle where they can't see you.)
+
+a3.2 Do not use spells on anyone for no roleplay reason. This is considered 'Freespelling'. You may also not use them to loophole systems in place (E.g. : Moving items that are for sell) (NOT IN GAME)
+
+a3.3 You can have rdm wars with friends but both sides need to agree to the fight and if something is lost it wont be refunded.
+
+a3.4 You may only have one account connected to any of our servers at once.
+
+a3.5 Impersonation is not allowed (pretending to be an admin, another player or in a clan)
+
+a3.6 You may not put any links in the chat. (Discord, YouTube, etc.)
+
+a3.7 Advertising in general and of Discord IDs and codes is prohibited.
+
+a3.8 Do not shoot players inside safe zones i.e. spawn and mining area.
+
+a3.9 Crossfire/missclick can still count as RDM. (Lost items can be refunded)
+
+a4.0 Do not make false reports.
+
+a4.1 If you mention Discord Ingame you will be muted for a day. You may only mention the server's discord.
+
+Building B.
+b1.0 All entrances must be visible
+
+b1.1 You may only own doors to one building, and must intend to build inside.
+
+b1.2 If you own the doors to a building, anyone that enters is KOS, but you are not.
+
+b1.3 Players entering a building area are KOS, but the builders are not.
+
+b1.4 You may not claim doors to a base that is already built, and cannot build where the doors are already bought.
+
+b1.5 When you’re building a base, place a ‘Building’ sign outside.
+  1.5B.You may not have a 'Building' sign while building in the Bank or Police Department.
+
+b1.6 You cannot have any raidable entities whilst using a building sign.
+
+b1.7 You’re only allowed 3 fading/keypad doors to get access to the whole of your base, this includes printers.
+
+b1.8 You cannot use props that make multiple doors that raiders need to go through.
+
+b1.9 Crouch and jump bases are disallowed.
+
+b2.0 Any entities should be accessible to interact with, but not necessarily steal.
+
+b2.1 Hidden keypads are allowed as long as near the door, and not materialize/colored to be nearly invisible or hard to see.
+
+b2.2 Fading/keypad doors should open for a minimum of 8 seconds and with a maximum delay of 2 seconds. (MAYBE CHANGE TO NO DELAY)
+
+b2.3 Your base must be raidable by everyone.
+
+b2.4 You cannot have props that cover over NPCs.
+
+b2.5 One way shooting is not allowed. (They must be able to shoot and see you too when you are shooting them, this does not include explosives and other AOE weapons)
+
+b2.6 You’re not allowed to RP when you’re building.
+
+b2.7 Prop climbing is not allowed, unless having a building sign up to assist building your base or as a City Worker to repair something temporarily. (NOT IN GAME)
+
+b2.8 World Glow / One way material is only allowed for viewing and not shooting(You also cannot use it to see where the player is to then shoot them trough a prop which is not one way.
+
+b2.9 Fading/Keypad doors should be clearly visible.
+
+b3.0 All KOS/Building Signs must be clearly visible in front of the base or area. You must use a visible font, colour and size (Minimum size of 50)
+
+b3.1 The following are the only allowed KOS signs. They may be combined. (Raiding, raiding tools out, past/on line(s), inside, on-ramp, on windows, shooting at base and messing with keypad).
+
+b3.2 You may not have stacked doors or no-collided layers of your entrance to prevent pick-locking or the destruction of defense doors (or even a portion of the doors). 
+Although you may have a separate entrance/exit for personal use.
+
+b3.3 You may have large sky bridges for the entrance or walkway to your base but it cannot be a kos zone.
+
+b3.4 You must be able to get through your base with ease E.g.: No keypad doors that are hard to pick lock.
+
+b3.5 You may not modify your base while being raided (E.g.: No-colliding) and you may not no-collide from your base to interact with the nearst NPC or to escape an RP Situation.
+
+b3.6 Bases that prevent players from leaving the same way they entered or impend their movement (e.g sliding bases) are not allowed.
+
+b3.7 Maze, Black, White, Mirror and Invisible bases are not allowed.(Any uni color base aren't allowed.)
+
+b3.8 Do not use materials that cause props to "flash"(using render FX in colour options), unless it's a DJ Floor.
+
+b3.9 You are only allowed only 1 players set of props per base. (E.g Can't have 2 part bases.) This does not include bases in the large area in residential where 2 players can build together.
+
+b4.0 You are allowed to base underwater but the entrance has to be above water.
+
+b4.1 When building in a tunnel 2 people must be able to pass side by side next to the base.
+
+
+
+Raiding C.
+c1.0 You’re not allowed to raid a base that has a building sign.
+
+c1.1 You're not allowed to raid the same base twice within 30 minutes.
+
+c1.2 Killing innocent bystanders while raiding is not permitted.
+
+c1.3 You have to leave the base asap if you have been longer in there for 10 minutes or if there are no valuables.
+
+c1.4 You may not teleport inside/outside a player's base or through their doors, E.g. Using the 'Obscuratio' spell; this is FailRP.
+
+c1.5 You have a limit of 1 hex shields per raid. (NOT IN GAME)
+
+c1.6 Raiding a base that you have agreed to base in is not permitted.
+
+c1.7 You are not allowed to use a taser during a raid. (Only for defence)
+
+
+
+
+Basing D.
+d1.0 Bank Manager or Security Guards (who are hired) are the only ones who can base in the bank. (The Bank Manager cannot kill any government officials.)
+
+d1.1 If you make a Clanbase, you must have 5 people (in the same clan)
+
+d1.2 You may not disconnect whilst a raid is occurring.
+
+d1.3 You may have only 5 people basing in one base, unless all members are in the same clan and all have the same clantag
+
+
+Mugging E.
+e1.0 Maximum mugging is $50,000
+
+e1.1 You must give the victim at least 10 seconds to drop the money, if they don’t, run away you may kill them.
+
+e1.2 There is a mug cooldown of 5 minutes and one of 30 minutes for the same person.
+
+e1.3 You must be infront the people you are mugging and confront them appropiately.
+
+e1.4 You must follow FearRP. (E.g. : You can not shoot the person that is mugging you.)
+
+e1.5 If you mug someone that already has a weapon out, they are allowed to kill you.
+
+e1.6 You may not group mug, or have multiple people mug someone at a time for more money.
+
+e1.7 You may not mug someone who is running.
+
+e1.8 Mugging people in the mining area and in the ocean who are mining is not allowed. This includes near the NPC and the edge of the safezone. (NOT IN GAME)
+
+e1.9 You may not mug law enforcement except for the President.
+
+e2.0 You must have a weapon out to mug a person, otherwise, this is classed as FailRP.
 
